9316176dbf7433675bbbe29315a13ce286f82f97
Jacobtkristensen <>
2023-05-31 15:31:17 +0200

    ConnectionManagerTest.java done

---
+++ src/test/java/kea/dk/devtool/utility/ConnectionManagerTest.java
@@ -0,0 +1,35 @@
+package kea.dk.devtool.utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectionManagerTest {
+    @Test
+    public void testGetConnection() {
+
+        // Test data
+        String DB_URL= "jdbc:mysql://localhost:3306/testdb";
+        String USER_IDE="testuser";
+        String PASSW="testpw";
+        // Perform the test
+        Connection connection = ConnectionManager.getConnection(DB_URL, USER_IDE, PASSW);
+
+        // Assert if the connection is not null
+        assertNotNull(connection, "Connection should not be null");
+        try {
+            /*  assertfalse forventer at condition er false.
+                connection.isClosed tjekker om forbindelsen er lukket.
+                ergo passer AssertNotNull, hvis forbindelsen ikke er lukket.
+            */
+            assertFalse(connection.isClosed(), "Connection is open");
+        } catch (SQLException e) {
+            fail("Exception: " + e.getMessage());
+        }
+    }
+}
